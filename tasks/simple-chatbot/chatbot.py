966677a7826c5571faea1273162c3dfa8e3931c9
Yurii Biurher <yuriy.burger@gmail.com>
2018-10-17 13:32:19 +0000

    simple bot

--- tasks/simple-chatbot/chatbot.py
+++ tasks/simple-chatbot/chatbot.py
@@ -1,92 +1,107 @@
 #!/usr/bin/env python3
 
-import os, sys, rl3
+import random, re, rl3
 
-# init RL3 engine
-engine = rl3.RL3Engine()
+class Answer():
+    def __init__(self, message=None, stop=False):
+        self.message = message
+        self.stop = stop
 
-# load 'intents' model
-engine.load('./intent.rl3c')
+def substitute(text, factsheet):
+    t = text
+    if factsheet is not None and len(factsheet) > 0:
+        for i in factsheet.get_facts():
+            t = t.replace('{%s}' % i.get_label(), i.get_value())
+    return t
 
-# init factsheet for background knowledge and user input
-facts = engine.create_factsheet()
+def make_answer(templates, subfacts, context):
+    random.shuffle(templates)
+    for i in templates:
+        try:
+            t = substitute(i, subfacts)
+            t = substitute(i, context)
+            if re.search(r"{[a-zA-Z0-9_\-]+}", t) is None:
+                return Answer(message=t)
+        except:
+            pass
 
-# default answer
-def answer_default(weight, subfacts):
-    return 'How can I help you?'
+    return None
 
-# answer to 'hello' intent
-def answer_hello(weight, subfacts):
-    return 'Hi! Nice to meet you.'
+class Actions():
+    def __init__(self):
+        pass
 
-#answer to 'goodbye' intent
-def answer_goodbye(weight, subfacts):
-    return 'Bye-bye!'
+    def bot_name(self, w, subfacts, conclusions, context):
+        return Answer(message="Yes?")
 
-# init answers db
-answers = {
-    '': answer_default,
-    'hello': answer_hello,
-    'goodbye': answer_goodbye
-}
+    def goodbye(self, w, subfacts, conclusions, context):
+        return Answer(message=random.choice(["Goodbye.", "Bye-bye."]), stop=True)
 
-# find best intent
-def get_intent(fs):
-    name = ''
-    weight = 0.0
-    subfacts = None
+    def hello(self, w, subfacts, conclusions, context):
+        return(make_answer(["Hello!", "Hi there!", "Hi."], subfacts, context))
 
-    if fs.has_fact('intent'):
+    def what_is_your_name(self, w, subfacts, conclusions, context):
+        return make_answer(["My name is {bot_name}.", "Call me {bot_name}."], subfacts, context)
+
+    def what_is_x(self, w, subfacts, conclusions, context):
+        return make_answer(["I know nothing about {X}."], subfacts, context)
+
+class Chatbot():
+    def __init__(self, name):
+        self.name = name
+        self.actions = Actions()
+        self.engine = rl3.RL3Engine()
+        self.engine.load('./intent.rl3c')
+
+    def get_intents(self, fs):
+        groups = dict()
         for i in fs.get_facts('intent'):
-            if i.get_weight() > weight:
-                name = i.get_value()
-                weight = i.get_weight()
-                if i.has_factsheet():
-                    subfacts = i.get_factsheet()
+            k = int(i.get_weight() * 1000)
+            if k not in groups:
+                groups[k] = []
+            groups[k].append((i.get_value(), i.get_weight(), i.get_factsheet() if i.has_factsheet() else None))
+        intents = []
+        for i in sorted(groups.keys(), reverse=True):
+            t = groups[i]
+            random.shuffle(t)
+            intents += t
+        return intents
 
-    return (name, weight, subfacts)
+    def process(self, user_input, context):
+        try:
+            facts = self.engine.create_factsheet_from_json(context) if context else self.engine.create_factsheet()
 
-# produce bot answer
-def think(user_input):
-    # init factsheet for conclusions
-    conclusions = engine.create_factsheet()
+            # reset 'bot name' fact
+            facts.retract_facts('bot_name')
+            facts.assert_simple_fact('bot_name', self.name)
 
-    # delete old input fact
-    facts.retract_facts('text')
+            # reset 'user input' fact
+            facts.retract_facts('text')
+            facts.assert_simple_fact('text', user_input)
 
-    # assert new input fact
-    facts.assert_simple_fact('text', user_input)
+            conclusions = self.engine.create_factsheet()
+            self.engine.run(facts, conclusions)
+            for name, weight, subfacts in self.get_intents(conclusions):
+                action = getattr(self.actions, name, None)
+                if action is not None:
+                    answer = action(weight, subfacts, conclusions, facts)
+                    if answer:
+                        facts.retract_facts('prior_intent')
+                        facts.assert_simple_fact('prior_intent', name)
+                        return (answer, facts.to_json())
+        except:
+            raise
 
-    # detect all intents
-    engine.run(facts, conclusions)
+        return (Answer(message='ouch...'), context)
 
-    # find best intent
-    name, weight, subfacts = get_intent(conclusions)
+chatbot = Chatbot('Bot')
 
-    stop = (name == 'goodbye')
-    if name in answers:
-        answer = answers[name](weight, subfacts)
-    else:
-        answer = answers[''](weight, subfacts)
-
-    return (answer, stop)
-
-def main():
-    user_input = ''
-
-    # main loop
-    while True:
-        # process user input
-        answer, stop = think(user_input)
-
-        # print answer
-        print('Bot: ' + answer)
-        print()
-
-        if stop:
-            break
-
-        # get new user input
-        user_input = input('You: ')
-
-main()
+print('Bot: Hello!\n')
+user_input = ''
+context = ''
+while True:
+    user_input = input('You: ')
+    answer, context = chatbot.process(user_input, context)
+    print('Bot: %s\n' % answer.message)
+    if answer.stop:
+        exit(0)
